924a875128e775e617051fa340e268f6bf614a7c
gichimux <gichimumwai1@gmail.com>
2020-02-24 23:16:54 +0300

    debugging

--- app/views.py
+++ app/views.py
@@ -7,20 +7,20 @@ from django.core.mail import send_mail, BadHeaderError
 # Create your views here.
 def index(request):
 	form = ContactForm()
-	request.method == 'POST'
-	name = request.POST.get('contact_name')
-	email = request.POST.get('contact_email')
-	content = request.POST.get('content')
+	# request.method == 'POST'
+	# name = request.POST.get('contact_name')
+	# email = request.POST.get('contact_email')
+	# content = request.POST.get('content')
 
-	if name and email and content:
-		try:
-			send_mail(name, content, email, ['dev.py.js@gmail.com'])
-		except BadHeaderError:
-			return HttpResponse('Invalid header found.') 
-		return HttpResponse('Email sent successfully')
+	# if name and email and content:
+	# 	try:
+	# 		send_mail(name, content, email, ['dev.py.js@gmail.com'])
+	# 	except BadHeaderError:
+	# 		return HttpResponse('Invalid header found.') 
+	# 	return HttpResponse('Email sent successfully')
 
-	else:
-		return render(request, 'index.html', {'form': form})
+	# else:
+	return render(request, 'index.html', {'form': form})
 
 def projects(request):
 	return render(request, 'projects.html', {})
